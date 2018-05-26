3aa7b1f2e660ef78a283f10b29ffdbd24ecfc97b
Kevin Tjiam <kevin@kevincharm.com>
2018-05-27 00:07:03 +0800

    improve StyledDiv syntax

--- src/common/styles.tsx
+++ src/common/styles.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react'
 
-export const StyledDiv: React.StatelessComponent<{ style: React.CSSProperties }> = ({ style, children }) => {
-    return <div style={style}>{children}</div>
+export const StyledDiv = (style: React.CSSProperties) => {
+    return (props: any): React.ReactElement<any> => {
+        return (
+            <div {...props} style={style}>
+                {props.children}
+            </div>
+        )
+    }
 }
--- src/content-scripts/App.styled.tsx
+++ src/content-scripts/App.styled.tsx
@@ -1,28 +1,19 @@
-import * as React from 'react'
 import { StyledDiv } from '../common/styles'
 
-export const StyledConsoleTitle: React.StatelessComponent = props =>
-    StyledDiv({
-        ...props,
-        style: {
-            position: 'absolute',
-            height: '36px',
-            width: '100%',
-            padding: '8px 12px',
-            fontSize: '16px',
-            backgroundColor: '#bdbdbd'
-        }
-    })
+export const StyledConsoleTitle = StyledDiv({
+    position: 'absolute',
+    height: '36px',
+    width: '100%',
+    padding: '8px 12px',
+    fontSize: '16px',
+    backgroundColor: '#bdbdbd'
+})
 
-export const StyledConsoleBody: React.StatelessComponent = props =>
-    StyledDiv({
-        ...props,
-        style: {
-            position: 'absolute',
-            height: '36px',
-            width: '100%',
-            padding: '8px 12px',
-            fontSize: '16px',
-            backgroundColor: '#bdbdbd'
-        }
-    })
+export const StyledConsoleBody = StyledDiv({
+    position: 'absolute',
+    height: '36px',
+    width: '100%',
+    padding: '8px 12px',
+    fontSize: '16px',
+    backgroundColor: '#bdbdbd'
+})
